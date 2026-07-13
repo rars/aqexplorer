a6d4b285b13407fa18332d281c4ff799c38c1770
Richard Russell <2265225+rars@users.noreply.github.com>
2026-07-13 16:09:51 +0100

    refactor: configure injection of APP_BASE_HREF

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,7 +12,7 @@ import { enGB } from 'date-fns/locale';
 import { routes } from './app.routes';
 import { DuckDbService } from './services/duckdb/duck-db';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
-import { APP_BASE_HREF } from '@angular/common';
+import { APP_BASE_HREF, PlatformLocation } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,11 +21,17 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     { provide: MAT_DATE_LOCALE, useValue: enGB },
     provideDateFnsAdapter(),
+    {
+      provide: APP_BASE_HREF,
+      useFactory: (platformLocation: PlatformLocation) => platformLocation.getBaseHrefFromDOM(),
+      deps: [PlatformLocation],
+    },
     provideAppInitializer(() => {
       const duckDbService = inject(DuckDbService);
       const baseHref = inject(APP_BASE_HREF, { optional: true }) || '/';
-      console.log(`Base href: ${baseHref}`);
-      const fileUrl = window.location.origin + baseHref + 'pm2.5.parquet';
+      const cleanBaseHref = baseHref.endsWith('/') ? baseHref : `${baseHref}/`;
+      console.log(`Base href: ${cleanBaseHref}`);
+      const fileUrl = window.location.origin + cleanBaseHref + 'pm2.5.parquet';
       return duckDbService.initDatabase(fileUrl);
     }),
   ],
